5d98df753ab580c5d28b475a7cffa0d213e7ab3e
Rheaxkhalifa-dotcom <rihab.x.khalifa@gmail.com>
2026-05-31 15:06:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Resume-Website/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
